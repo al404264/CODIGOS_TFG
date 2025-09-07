33245b8ae0fa7e5fc27f2a07b32835ec9196b188
Usuario <al404264@uji.es>
2025-09-07 17:16:53 +0200

    des

--- src/EscapeFunciones.java
+++ src/EscapeFunciones.java
@@ -5,14 +5,14 @@ import java.awt.image.BufferedImage;
 public class EscapeFunciones extends JPanel {
 
     private double xmin, xmax, ymin, ymax;
-    private int maxIter = 1000;
+    private int maxIter = 150;
     private double escape = 2.0;
 
     private BufferedImage img;
 
     // Valor fijo de c (puedes cambiarlo directamente en el código)
-    private double cRe = -0.7;
-    private double cIm = 0.27015;
+    private double cRe = -0.39054087021840056;
+    private double cIm = -0.5867879073469685;
 
     public EscapeFunciones(int width, int height,
                            double xmin, double xmax,
